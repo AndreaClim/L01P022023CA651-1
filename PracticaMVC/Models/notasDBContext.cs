93b0c4c16c24e1306475d8bd09f9926309dfafb4
miche <miche@DESKTOP-EEADCHD>
2025-03-27 19:32:28 -0600

    creacion de contexto

---
+++ PracticaMVC/Models/notasDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class notasDBContext : DbContext
+    {
+        public notasDBContext(DbContextOptions<notasDBContext> options) : base(options)
+        {
+        }
+        public DbSet<alumnos> alumnos { get; set; }
+        public DbSet<departamentos> departamentos { get; set; }
+        public DbSet<facultades> facultades { get; set; }
+        public DbSet<materias> materias { get; set; }
+    }
+}
